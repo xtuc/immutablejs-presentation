77937ec7df358df4e8953cd0342674b6e119f2d1
Sven SAULEAU <sven.sauleau@xtuc.fr>
2016-02-03 23:32:11 +0100

    modif

--- test.js
+++ test.js
@@ -1,7 +1,25 @@
-var Immutable = require("immutable")
+import Immutable from "immutable"
 
-var foo = Immutable.List.of(1, 2, 3);
+export default class IPv4Packet extends Immutable.Record({
+	Version: 4,
+	IPHeaderLength: null,
+	TypeofService: null,
+	SizeofDatagram: null,
+	Identification: null,
+	Flags: null,
+	FragmentationOffset: null,
+	TimeToLive: null,
+	Protocol: null,
+	HeaderChecksum: null,
+	SourceAddress: null,
+	DestinationAddress: null
+}) {
+	//
+}
 
-foo = foo.set(2, "bar")
+var res = new IPv4Packet()
+				.set("Version", 5);
 
-console.log(foo.inspect());
+var resJSON = res.toJSON();
+
+console.log(new IPv4Packet(resJSON));
