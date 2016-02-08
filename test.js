87e88fc3913b92239f9d93e9b863207293c1c143
Sven SAULEAU <sven.sauleau@xtuc.fr>
2016-02-09 00:23:22 +0100

    perf slides

--- test.js
+++ test.js
@@ -1,8 +1,50 @@
 import Immutable from "immutable"
 
-var oddSquares = Immutable.List.of(1,2,3,4,5,6,7,8)
-                                .filter(x => x % 2)
-                                .map(x => x * x);
+var faker = require('faker');
+var sleep = require('sleep');
 
-// Only calls filter twice and map once
-console.log(oddSquares.get(1)); // 9
+const times = [];
+const iteration = 10000000;
+
+function benchPlain(object) {
+
+	for (var i = 0; i < iteration; i++) {
+		const time = Date.now();
+
+		/**
+		 * Operation
+		 */
+		object === (object[1] = 0)
+
+		times.push(Date.now() - time);
+	}
+}
+
+function benchImmutable(object) {
+
+	for (var i = 0; i < iteration; i++) {
+		const time = Date.now();
+
+		/**
+		 * Operation
+		 */
+		object.equals(object.set(1, 0))
+
+		times.push(Date.now() - time);
+	}
+}
+
+for (var i = 0; i < 100; i++) {
+	var object = {};
+
+	for (var i = 0; i < 1000; i++) {
+		object[i] = faker.hacker.noun(); 
+	}
+
+	benchPlain(object)
+	// benchImmutable(Immutable.fromJS(object))
+}
+
+const total = times.reduce((acc, e) => acc + e);
+
+console.log("bench %s s (%s iterations)", total / times.length * 1000, times.length / (1000 * 1000) + "M")
