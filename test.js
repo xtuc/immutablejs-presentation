ceb7b45123f7b5311c0acb9bccb7ccd9bc31025c
Sven SAULEAU <sven.sauleau@xtuc.fr>
2016-02-10 18:55:57 +0100

    tests, who cares

--- test.js
+++ test.js
@@ -1,8 +1,32 @@
-import Immutable from "immutable"
-import { compose } from "transducers.js"
+var Immutable = require("immutable")
 
-const double = (x) => x * 2
-const increment = (x) => x + 1
+var test = Immutable.List.of(1);
+var test2 = Immutable.List.of(1);
 
-const foo = Immutable.List.of(1,2,3);
-var a = foo.map(compose(double, increment));
+console.log(test.hashCode(), "===", test2.hashCode(), test.hashCode() === test2.hashCode())
+// import { compose } from "transducers.js"
+
+// const double = (x) => x * 2
+// const increment = (x) => x + 1
+
+// const foo = Immutable.List.of(1,2,3);
+// var a = foo.map(compose(double, increment));
+
+// var a = {};
+// var b = {};
+// var debug = true;
+
+// a.hash = "test";
+// b.hash = "test";
+
+// Object.defineProperty(a, "a", { get: function() { return 0; },
+// 	                            set: function(val) { },
+//                                 enumerable: debug,
+//                                 configurable: true });
+
+// Object.defineProperty(b, "a", { get: function() { return 0; },
+// 	                            set: function(val) { },
+//                                 enumerable: debug,
+//                                 configurable: true });
+
+// console.log(JSON.stringify(a), "===", JSON.stringify(b), "->", a === b)
