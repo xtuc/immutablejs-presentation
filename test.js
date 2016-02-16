382e9447ea414bbc52fd9924fbbf3dd9eb471163
Sven SAULEAU <sven.sauleau@xtuc.fr>
2016-02-16 14:57:21 +0100

    Remove comparaison slides

--- test.js
+++ test.js
@@ -1,8 +1,27 @@
 var Immutable = require("immutable")
 
-const foo = Immutable.Map({ a: "0", b: "foo" })
+function Foo(str) { this.str = str; }
 
-console.log(Immutable.is(foo, foo))
-console.log(foo.equals(foo))
+Foo.prototype.hashCode = function() {
+	return Math.random();
+}
 
-console.log(foo.hashCode());
+Foo.prototype.equals = function(obj) {
+	return this.str === obj.str;
+}
+
+var test = new Foo("test");
+
+console.log(test.equals(new Foo("test")), "-->true");
+
+var bar = Immutable.Set.of(test)
+
+console.log(bar.inspect())
+console.log(bar.contains(new Foo("test")));
+
+// var bar = new Foo("bar");
+
+// // console.log(bar.valueOf(), "hashCode", bar.hashCode);
+
+// console.log(bar.valueOf);
+// console.log(Immutable.is(bar, new Foo("bar")))
